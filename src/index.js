23ebe6ba0316695cf0fd1d68f7a7d32bd8684460
Adam Gajzlerowicz <nelf86@gmail.com>
2018-12-09 16:15:40 +0100

    poc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import io from 'socket.io-client'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const socket = io('http://localhost:3005', {
+// })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const socket = io('http://localhost:3005', {
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        authentication: 'blah'
+      }
+    }
+  }
+})
+
+socket.on('exception', e => {
+  console.log(e)
+})
+
+socket.on('connect', function() {
+  console.log('connect')
+})
+socket.on('sync', function(data) {
+  console.log('event', data)
+})
+
+socket.on('disconnect', function() {
+  console.log('disconnect')
+})
